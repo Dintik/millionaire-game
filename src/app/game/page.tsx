a6f6e0d50a61ecaf8303bc5881bdcb90cf8ce90c
Dintik <chailo.artem@gmail.com>
2025-05-11 16:40:12 +0300

    setup pages structure

---
+++ src/app/game/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function GamePage() {
+  return (
+    <>
+      <h1>Game Page</h1>
+      <Link href='/result'>Finish</Link>
+    </>
+  )
+}
---
+++ src/app/result/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function ResultPage() {
+  return (
+    <>
+      <h1>Result Page</h1>
+      <Link href='/game'>Try again</Link>
+    </>
+  )
+}
