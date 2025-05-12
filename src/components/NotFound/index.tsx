8b1b20063bea4f082b6ace1dd7519a01bfa590ab
Dintik <chailo.artem@gmail.com>
2025-05-12 18:21:52 +0300

    add NotFoundPage

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import { LinkButton } from '@/components/ui/Buttons'
+import styles from './styles.module.scss'
+
+export function NotFound() {
+  return (
+    <main className={styles.notFound}>
+      <h1>Oops! Page not found</h1>
+      <p>
+        The page you&apos;re looking for doesn&apos;t exist or may have been
+        removed. No worries! You can safely return by clicking the button below.
+      </p>
+      <LinkButton>Go back to safety</LinkButton>
+    </main>
+  )
+}
